c162bbf3941050dfb4bcbd98ac7cb0ca4cf1e8ec
Wilscos <43081060+Wilscos@users.noreply.github.com>
2022-02-01 20:09:56 +0100

    Fixed config

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
-DATA_PATH = 'data'
-PLOT_PATH = 'plots'
+import os
+import sys
+
+
+MASTER = sys.path[1]
+DATA_PATH = os.path.join(MASTER, 'data')
+PLOT_PATH = os.path.join(MASTER, 'plots')
+PDF_PATH = os.path.join(DATA_PATH, 'articles_pdf')
