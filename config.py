35d6dbf50d4fa4e033aebc3a3a32c8b101a4bb2f
Wilscos <43081060+Wilscos@users.noreply.github.com>
2022-02-03 15:43:51 +0100

    Added txts' location to config

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ MASTER = sys.path[1]
 DATA_PATH = os.path.join(MASTER, 'data')
 PLOT_PATH = os.path.join(MASTER, 'plots')
 PDF_PATH = os.path.join(DATA_PATH, 'articles_pdf')
+TXT_PATH = os.path.join(DATA_PATH, 'texts')
