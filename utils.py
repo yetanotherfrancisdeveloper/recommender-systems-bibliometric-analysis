2abe22f9d001c25492f72361a154e52e0d4389b5
Wilscos <43081060+Wilscos@users.noreply.github.com>
2022-02-03 19:39:36 +0100

    Added function to add names of pdf files to the dataframe of results from LDA

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import os
 import json
+import numpy as np
+import re
 import pandas as pd
-from config import DATA_PATH
+from config import DATA_PATH, PDF_PATH
 
 
-def read_data():    # TODO: Make 'df.csv' customisable
+def read_data():
     if not os.path.isdir(DATA_PATH):
         os.mkdir(DATA_PATH)
 
@@ -16,3 +18,14 @@ def read_data():    # TODO: Make 'df.csv' customisable
 def obj_to_file(a_list, file_name):
     with open(os.path.join(DATA_PATH, f'{file_name}.txt'), 'w') as file:
         json.dump(a_list, file, ensure_ascii=False)
+
+
+def get_pdf_from_txt_name(df):
+    txt_names = df['file'].tolist()
+    pdf_files = np.array(os.listdir(PDF_PATH))
+    txt_idxes = [int(re.findall('\d+', txt)[0]) - 1 for txt in txt_names]
+    our_pdf_files = pdf_files[txt_idxes]
+    # arxiv.org/abs/name_pdf.pdf
+    df['pdf_file'] = our_pdf_files
+
+    return df
