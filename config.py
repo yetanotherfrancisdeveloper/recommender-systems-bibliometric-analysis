08061b5c5d0232c0a0fe9b94cf11a00ea5f852e9
Wilscos <43081060+Wilscos@users.noreply.github.com>
2022-02-07 15:37:00 +0100

    Updated config.py
    Update to better identify the MASTER path and to add the path to the pos_tags saved when running

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
+import pathlib
 import sys
 
 
-MASTER = sys.path[1]
+MASTER = pathlib.Path().absolute()
 DATA_PATH = os.path.join(MASTER, 'data')
 PLOT_PATH = os.path.join(MASTER, 'plots')
 PDF_PATH = os.path.join(DATA_PATH, 'articles_pdf')
 TXT_PATH = os.path.join(DATA_PATH, 'texts')
+POS_PATH = os.path.join(DATA_PATH, 'pos_tags.json')
