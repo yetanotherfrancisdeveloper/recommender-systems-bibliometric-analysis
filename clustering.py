e5e946ff1dc9a419573eb986ee2d8d9c976ce531
Wilscos <43081060+Wilscos@users.noreply.github.com>
2022-02-07 15:34:42 +0100

    Moved functions from 'transformer.py' to 'clustering.py' and 'main.py'
    Made 'main.py' the only file needed to be run in order to execute everything

--- clustering.py
+++ clustering.py
@@ -1,32 +1,95 @@
-import os
 import numpy as np
-import pandas as pd
-from sklearn.cluster import DBSCAN
-from utils import DATA_PATH
+import os
+from config import DATA_PATH, PLOT_PATH
+from keybert import KeyBERT
+from matplotlib import pyplot as plt
 from sentence_transformers import SentenceTransformer
+from sklearn.cluster import DBSCAN
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.manifold import TSNE
 
 
-def get_sentence_embeddings(sentences, model = 'all-MiniLM-L6-v2'):
+def get_sentence_embeddings(sentences, model='all-MiniLM-L6-v2'):
     sbert_model = SentenceTransformer(model)
     embeddings = sbert_model.encode(sentences)
+
     return embeddings
 
 
-def main():
-    df = pd.read_csv(os.path.join(DATA_PATH, 'df.csv'))
-    if not os.path.exists(os.path.join(DATA_PATH, 'embeddings.npy')):
-        summaries = df['summary']
-        embeddings_ndarray = get_sentence_embeddings(summaries)
-        np.save(os.path.join(DATA_PATH, 'embeddings.npy'), embeddings_ndarray)
-    else:
-        embeddings_ndarray = np.load(os.path.join(DATA_PATH, 'embeddings.npy'))
+def dbscan_clustering(df, embeddings_ndarray):
     clustering = DBSCAN(eps=0.5, min_samples=500, metric='cosine').fit(embeddings_ndarray)
     clusters = clustering.labels_
     print('Cluster labels', np.unique(clusters))
     df['cluster'] = clusters
     print(df['cluster'].value_counts())
-    df.to_csv(os.path.join(DATA_PATH, 'df.csv'))
+    df.to_csv(os.path.join(DATA_PATH, 'df_dbscan_results.csv'))
 
 
-if __name__ == '__main__':
-    main()
+def generate_keywords(docs):
+    keywords_model = KeyBERT()
+    keywords = list()
+
+    for idx, summary in enumerate(docs):
+        if idx % 50 == 0:
+            print(f'Processing paper {idx+1}')
+        keywords.append(keywords_model.extract_keywords(summary, keyphrase_ngram_range=(1, 2), stop_words=None))
+
+    return keywords
+
+
+def get_keywords_list(keyword_score_list, mode = 'all'):
+    keywords_list = list()
+    if mode == 'all':
+        for keywords_per_paper in keyword_score_list:
+            keywords = list()
+            for keyword_score in keywords_per_paper:    # I think it was meant to be nested like this?
+                keywords.append(keyword_score[0])
+            keywords_list.append(keywords)
+    elif mode == 'best':
+        for keywords_per_paper in keyword_score_list:
+            keywords = [keyword_score[0] for keyword_score in keywords_per_paper]
+            scores = [keyword_score[1] for keyword_score in keywords_per_paper]
+            best_keyword = keywords[np.argmax(scores)]
+            keywords_list.append(best_keyword)
+
+    return keywords_list
+
+
+def lda_clustering(df, n_topics=5, min_df=0.05, max_df=0.95, column='summary'):
+    # Create Count Vectorizer instance and Document X Term matrix (dtm)
+    print('\nCreating Count Vectorizer and Document X Term matrix ...')
+    cv = CountVectorizer(max_df=max_df, min_df=min_df, stop_words="english")
+    dtm = cv.fit_transform(df[column])
+
+    # Creat the model
+    print('\nCreating LDA model ...')
+    lda_model = LatentDirichletAllocation(n_components=n_topics, max_iter=30, random_state=2022)
+    # Fit model
+    print('\nFitting the LDA model ...')
+    lda_model.fit(dtm)
+
+    for i, topic in enumerate(lda_model.components_):
+        print(f"THE TOP {20} WORDS FOR TOPIC #{i}")
+        print([cv.get_feature_names_out()[index] for index in topic.argsort()[-20:]])
+        print("\n")
+
+    final_topics = lda_model.transform(dtm)
+    df["topics"] = final_topics.argmax(axis=1)
+
+    # TSNE for visualization of the clusters
+    print('\nCreating and fitting TSNE to visualize clusters in 2D ...')
+    tsne_model = TSNE(n_components=2, verbose=1, random_state=0, angle=.99, init='pca')
+    tsne_lda = tsne_model.fit_transform(final_topics)
+    print('\nt-SNE shape: ', tsne_lda.shape)
+
+    for g in np.unique(df.topics):
+        label_idx = np.where(df.topics == g)
+        tsne_lda_label = tsne_lda[label_idx]
+        plt.scatter(x=tsne_lda_label[:, 0], y=tsne_lda_label[:, 1], cmap='turbo', label=f'Topic {g}')
+
+    plt.legend()
+    plt.savefig(f'{PLOT_PATH}/t-SNE_LDA_(topics={n_topics},min_df={min_df},max_df={max_df}.png')
+    plt.close()
+
+    return df
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import argparse
+import json
 import nltk
 import numpy as np
 import os
 import pandas as pd
-from config import PLOT_PATH
-from matplotlib import pyplot as plt
-from preprocessing import pre_processing, add_word_count, corpus_words_frequency, get_words_for_pos, get_pos_tag
-from utils import read_data
+from clustering import (dbscan_clustering, generate_keywords, get_keywords_list,
+                        get_sentence_embeddings, lda_clustering)
+from config import DATA_PATH, POS_PATH, TXT_PATH
+from plotter import bar_plot, plot_publications_series, plot_words_distribution
+from preprocessing import (add_word_count, clean_corpus_by_pos, compute_tf_idf,
+                           corpus_words_frequency, get_pos_tag, get_words_for_pos,
+                           pre_processing, remove_most_frequent_words, remove_short_texts, remove_short_words)
+from tqdm import tqdm
+from utils import from_json_to_list_tuples, get_pdf_from_txt_name, obj_to_file, read_data, save_json
 
 nltk.download('stopwords')
 nltk.download('wordnet')
@@ -13,91 +20,152 @@ nltk.download('omw-1.4')
 pd.set_option('display.max_columns', None)
 
 
-def bar_plot(words_list: list, frequencies: list, width=0.35, top_k=10):
-    x = np.arange(top_k)
-    plt.bar(x, frequencies[:top_k], width=width)
-    plt.xticks(x, words_list[:top_k], rotation=25)
+def main(skip_data_viz=True, clustering_model='lda'):
+    if not skip_data_viz:
+        df = read_data()
+        print('Dataframe columns:\n')
+        for col in df.columns:
+            print(f'- {col}')
 
-    if not os.path.isdir(PLOT_PATH):
-        os.mkdir(PLOT_PATH)
+        print('\nFirst 5 rows of dataframe: ')
+        print(df.head(5))
 
-    plt.savefig(os.path.join(PLOT_PATH, f'top_{top_k}_words_by_frequency.png'))
-    plt.close()
+        # Adding word count column
+        df = add_word_count(df, 'summary')
+        # Pre-process abstracts
+        print('\nPre-processing abstracts ...')
+        corpus = pre_processing(df, 'summary')
 
+        # Get words' frequency
+        print('\nGetting words\' frequency ...')
+        words_count = corpus_words_frequency(corpus)
+        words_list = list(words_count.keys())
+        # Get tags for unique words
+        print('\nGetting words for POS tag ...')
+        tagged_words = get_pos_tag(words_list)
+        nn_words = get_words_for_pos(tagged_words, 'NN')
+        nns_words = get_words_for_pos(tagged_words, 'NNS')
+        nnp_words = get_words_for_pos(tagged_words, 'NNP')
 
-def plot_publications_series(df, remove_years=np.array([])):
-    # Creating column 'year'
-    df['year'] = df.published.apply(lambda x: x[-4:])
+        # Concatenating all lists of nouns
+        noun_words = nn_words + nns_words + nnp_words
 
-    # Getting data in dictionary
-    data = {year: len(df[df.year == year]) for year in df.year.unique()}
+        print('\nGetting words to plot ...')
+        words_to_plot = {k: v for k, v in words_count.items() if k in noun_words}
+        sorted_words_to_plot = sorted(words_to_plot.items(), key=lambda x: x[1], reverse=True)
+        words_to_plot = dict(sorted_words_to_plot)
+        # Bar plot of top k words
+        bar_plot(list(words_to_plot.keys()), list(words_to_plot.values()))
+        # Bar plot of publications per year
+        plot_publications_series(df, remove_years=np.array(['2022']))
 
-    # Remove years if given
-    if remove_years:
-        for year in remove_years:
-            try:
-                del data[year]
-            except KeyError:
-                print(f"The year {year} wasn't in the data! Check again.")
-                continue
+    if clustering_model == 'keybert':
+        df = read_data()
+        if not os.path.exists(os.path.join(DATA_PATH, 'keywords.txt')):
+            docs = df['summary']
+            keywords = generate_keywords(docs)
+            obj_to_file(keywords, 'keywords')
+        else:
+            with open(os.path.join(DATA_PATH, 'keywords.txt')) as file:
+                keywords = json.load(file)
 
-    # Sorting dictionary by year in ascending order
-    sorted_data = sorted(data.items(), key=lambda x: x[0])
-    sorted_data = dict(sorted_data)
+        keywords_list = get_keywords_list(keywords, mode='best')
+        obj_to_file(keywords_list, 'best_keywords')
+        df['keywords'] = keywords_list
+        df.to_csv(os.path.join(DATA_PATH, 'df_extended.csv'))
 
-    # Getting from and to years
-    from_year = min(list(sorted_data.keys()))
-    to_year = max(list(sorted_data.keys()))
+    elif clustering_model == 'dbscan':
+        df = read_data()
+        if not os.path.exists(os.path.join(DATA_PATH, 'embeddings.npy')):
+            summaries = df['summary']
+            embeddings_ndarray = get_sentence_embeddings(summaries)
+            np.save(os.path.join(DATA_PATH, 'embeddings.npy'), embeddings_ndarray)
+        else:
+            embeddings_ndarray = np.load(os.path.join(DATA_PATH, 'embeddings.npy'))
 
-    # Bar plot
-    plt.bar(np.arange(len(sorted_data.keys())), list(sorted_data.values()), width=0.35)
-    plt.xticks(np.arange(len(sorted_data.keys())), list(sorted_data.keys()))
+        dbscan_clustering(df, embeddings_ndarray)
 
-    if not os.path.isdir(PLOT_PATH):
-        os.mkdir(PLOT_PATH)
+    elif clustering_model == 'lda':
+        # Clustering with the Latent Dirichlet Allocation (LDA) algorithm
+        txt_list = os.listdir(TXT_PATH)
+        txt_str_list = []
 
-    plt.savefig(os.path.join(PLOT_PATH, f'bar_plot_publications_per_year_{from_year}-{to_year}.png'))
-    plt.close()
+        print('\nOpening .txt files for the analysis ...')
+        for txt in tqdm(txt_list):
+            with open(os.path.join(TXT_PATH, txt), encoding='utf-8') as f:
+                txt_str_list.append(f.read())
 
+        txt_df = pd.DataFrame({'article': txt_str_list, 'file': txt_str_list})
+        print('\nPre-processing articles for LDA ...')
+        corpus = pre_processing(txt_df, 'article')
 
-def main():
-    df = read_data()
-    print('Dataframe columns:\n')
-    for col in df.columns:
-        print(f'- {col}')
+        # Removing short texts
+        print('\nRemoving short texts ...')
+        corpus, short_texts_idxes = remove_short_texts(corpus)
 
-    print('\nFirst 5 rows of dataframe: ')
-    print(df.head(5))
+        # Remove texts from
+        try:
+            for idx in range(len(short_texts_idxes)):
+                txt_list.pop(short_texts_idxes[idx] - idx)
+        except (IndexError, TypeError) as e:
+            print(f'{e}: check the threshold or the list!')
 
-    # Adding word count column
-    df = add_word_count(df, 'summary')
-    # Pre-process abstracts
-    print('\nPre-processing abstracts ...')
-    corpus = pre_processing(df, 'summary')
+        # Removing short words
+        print('\nRemoving short words ...')
+        corpus = remove_short_words(corpus)
 
-    # Get words' frequency
-    print('\nGetting words\' frequency ...')
-    words_count = corpus_words_frequency(corpus)
-    words_list = list(words_count.keys())
-    # Get tags for unique words
-    print('\nGetting words for POS tag ...')
-    tagged_words = get_pos_tag(words_list)
-    nn_words = get_words_for_pos(tagged_words, 'NN')
-    nns_words = get_words_for_pos(tagged_words, 'NNS')
-    nnp_words = get_words_for_pos(tagged_words, 'NNP')
+        print('\nGetting words\' frequency ...')
+        words_count = corpus_words_frequency(corpus)
+        words_list = list(words_count.keys())
 
-    # Concatenating all lists of nouns
-    noun_words = nn_words + nns_words + nnp_words
+        print('\nGetting words for POS tag ...')
+        if os.path.exists(POS_PATH):
+            tagged_words = from_json_to_list_tuples(json_path=POS_PATH)
+            nn_words = get_words_for_pos(tagged_words, 'NN')
+        else:
+            tagged_words = get_pos_tag(words_list)
+            save_json(out_path=POS_PATH, data=tagged_words)
+            nn_words = get_words_for_pos(tagged_words, 'NN')
 
-    print('\nGetting words to plot ...')
-    words_to_plot = {k: v for k, v in words_count.items() if k in noun_words}
-    sorted_words_to_plot = sorted(words_to_plot.items(), key=lambda x: x[1], reverse=True)
-    words_to_plot = dict(sorted_words_to_plot)
-    # Bar plot of top k words
-    bar_plot(list(words_to_plot.keys()), list(words_to_plot.values()))
-    # Bar plot of publications per year
-    plot_publications_series(df, remove_years=np.array(['2022']))
+        # Concatenating all lists of nouns
+        noun_words = nn_words
+
+        # Getting new corpus by removing non POS words
+        print('\nGetting new corpus with only POS tag words ...')
+        new_corpus = clean_corpus_by_pos(corpus, noun_words)
+        print('\nGetting words\' frequency ...')
+        new_words_count = corpus_words_frequency(new_corpus)
+        plot_words_distribution(new_words_count)
+
+        # Remove most frequent words
+        new_corpus_clean = remove_most_frequent_words(new_corpus, new_words_count)
+
+        # LDA clustering with new corpus
+        txt_df = pd.DataFrame({'article': new_corpus_clean})
+        results_df = lda_clustering(txt_df, column='article')
+        # Computing top 20 words by tf-idf
+        print('\nComputing tf-idf ...')
+        top_tf_idf_words, tf_idf_results = compute_tf_idf(results_df)
+        # Printing top 20 words by tf-idf
+        topics = results_df.topics.unique()
+        topics.sort()
+        for topic in topics:
+            print(f"\nTF-IDF TOP {20} WORDS FOR TOPIC #{topic}")
+            print(top_tf_idf_words[topic])
+
+        # Adding .txt and .pdf files' names to csv and saving it
+        print('\nSaving csv with results ...')
+        results_df['file'] = txt_list
+        results_df = get_pdf_from_txt_name(results_df)
+        results_df.to_csv(f'{DATA_PATH}/lda_results.csv')
 
 
 if __name__ == '__main__':
     main()
+    # Adding parsing of arguments for terminal
+    parser = argparse.ArgumentParser(description='Clustering papers on recommender systems into topics')
+    parser.add_argument('-skip_data_viz', type=bool, help='Creates and saves plots on the papers data. '
+                                                          'Value to use: True | False')
+    parser.add_argument('-clustering_model', type=str, help='Model to use in order to cluster data. '
+                                                            'Values to use: "lda" | "dbscan" | "keybert"')
+    args = parser.parse_args()
